0dccf8f81d65ac4b1cc59a7dce1af737113fabd5
Victor Sandu <vyctor661@gmail.com>
2025-09-13 22:04:53 +0300

    Swap GitHub and Patreon links in header actions for better visibility and support options

--- src/App.tsx
+++ src/App.tsx
@@ -176,15 +176,6 @@ function App(): React.ReactElement | null {
                 </div>
                 <div className="header-actions">
                     <a
-                        className="header-btn header-btn--patreon"
-                        href="https://www.patreon.com/cw/vycdev"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <i className="fa-brands fa-patreon" aria-hidden />
-                        <span>Support me</span>
-                    </a>
-                    <a
                         className="header-btn header-btn--github"
                         href="https://github.com/vycdev/StrataPaint"
                         target="_blank"
@@ -192,6 +183,15 @@ function App(): React.ReactElement | null {
                     >
                         <i className="fa-brands fa-github" aria-hidden />
                         <span>GitHub</span>
+                    </a>
+                    <a
+                        className="header-btn header-btn--patreon"
+                        href="https://www.patreon.com/cw/vycdev"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fa-brands fa-patreon" aria-hidden />
+                        <span>Support me</span>
                     </a>
                 </div>
             </header>
